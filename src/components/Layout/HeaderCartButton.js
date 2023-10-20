e706f241b33cbd7fe6b134dc31626140d19608e6
abhi <abhigowdabs154@gmail.com>
2023-10-20 12:49:01 +0530

    useREducer hook in cartProvider

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,32 @@
-
+import React,{useContext} from 'react';
 import classes from './HeaderCartButton.module.css'
 import CartIcon from '../Cart/CartIcon';
 import './HeaderCartButton.module.css'
 
+import CartContext from '../../store/Cart-context';
+
+
+ 
 
 const HeaderCartButton=(props)=>{
+
+const cartCtx=useContext(CartContext)
+
+const noOfTotalItem=cartCtx.items.reduce((curNumber,item)=>{
+
+  return curNumber+item.amount;
+},0)
+
+// above reduce method reduces the repeated item into single , like 3 icecreem and 2 hot dog 
+// is still 1 icreem ob and 1 hot dog , 
+//curnumbwe is a func tht calls , item is a item to be reduced
+
 
     return(
         <button className={classes.button} onClick={props.onClick} >
         <span className={classes.icon}><CartIcon /> </span>
         <span className={classes.cart}>Your Cart</span>
-          <span className={classes['badge']}>0</span>
+          <span className={classes['badge']}>{noOfTotalItem}</span>
         </button>
      );
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,62 @@
+import CartContext from "./Cart-context";
+import { useReducer } from "react";
+
+
+
+const defaultCartState={
+     items: [],
+        totalAmount: 0,
+      
+
+} 
+
+const cartReducer=(state,action)=>{
+
+    if(action.type==='ADD')
+    {
+const updatedItems=state.items.concat(action.item);
+const updatesTotalAmount=state.totalAmount+action.item.price*action.item.amount;
+
+return{
+    items:updatedItems,
+    totalAmount:updatesTotalAmount,
+}
+
+}
+    return defaultCartState;
+}
+ 
+
+
+const CartProvider = props => {
+
+    const [cartState,disPatchAction]=useReducer(cartReducer,defaultCartState);
+
+   
+
+    const addItemToCartHandler=(item)=>{
+disPatchAction({type:"ADD",item:item})
+    
+};
+
+    const removeItemFromCartHandler=(id)=>{
+
+        disPatchAction({type:'ACTION',id:id})
+    }
+    
+    const cartContext={
+        items:cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem:removeItemFromCartHandler,
+    };
+    
+
+
+
+  return <CartContext.Provider value={cartContext}>
+                            {props.children}
+                                         </CartContext.Provider>;
+};
+
+export default CartProvider;
